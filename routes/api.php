abaaa5a030f348e13014fb36295c7fed45fe977e
Abidmo07 <ma.abid@esi-sba.dz>
2025-06-04 19:59:45 +0100

    finishing the auth system using laravel fortify and sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,36 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/email/verify/{id}/{hash}', function (Request $request, $id, $hash) {
+   if(!$request->hasValidSignature()){
+            return response()->json([
+                "message"=> "Invalide or expired verification code"
+            ],400);
+         }
+         $user=User::find($id);
+         if(!$user){
+            return response()->json([
+                "message"=> "user not found"
+            ],400);
+         }
+         if(!$user->hasVerifiedEmail()){
+            $user->markEmailAsVerified();
+            return response()->json([
+                "message"=> "Email verified with success",
+                "user"=>$user
+            ]);
+         }
+
+
+
+        return response()->json([
+            "message"=> "Email already verified"
+        ],400);
+})->name('verification.verify');
